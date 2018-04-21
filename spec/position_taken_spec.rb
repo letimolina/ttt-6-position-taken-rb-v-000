4e6ac2ce1957ef729aad15844b23aa749f424460
Leticia Molina <letimolina@gmail.com>
2018-04-21 21:09:29 +0000

    automatically backed up by learn

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/position_taken.rb'
 
 describe '#position_taken? in lib/position_taken.rb' do
   it 'accepts a board and the index to check as arguments' do
-    board = []
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
 
     expect{position_taken?(board, index)}.to_not raise_error
